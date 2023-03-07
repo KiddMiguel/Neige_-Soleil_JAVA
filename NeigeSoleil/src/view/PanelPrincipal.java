2ef08d2fd546ef6115590d6b8444c79e628b9d0a
chef-Cuisto <116522751+chef-Cuisto@users.noreply.github.com>
2023-03-07 21:55:35 +0100

    test1

---
+++ NeigeSoleil/src/view/PanelPrincipal.java
@@ -0,0 +1,22 @@
+package view;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class PanelPrincipal extends JPanel {
+	
+	protected JLabel titre = new JLabel("Mon profil");
+	
+	public PanelPrincipal(){
+	    this.setBounds(15, 20, 1500, 800);
+	    this.setLayout(null);
+	    
+	    
+	    
+	    this.setVisible(false);
+		
+	    
+	}
+	
+
+}
