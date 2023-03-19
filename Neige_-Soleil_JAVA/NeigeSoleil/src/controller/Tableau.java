616553de93b523c1ce782494424ad1bca35064de
KiddMiguel <103890453+KiddMiguel@users.noreply.github.com>
2023-03-19 16:51:29 +0100

    Appartement
    Okay fini !

--- Neige_-Soleil_JAVA/NeigeSoleil/src/controller/Tableau.java
+++ Neige_-Soleil_JAVA/NeigeSoleil/src/controller/Tableau.java
@@ -49,4 +49,37 @@ public class Tableau extends AbstractTableModel{
 		this.donnees = matrice ; //recopie de matrices.
 		this.fireTableDataChanged(); // mise à jour des données. 
 	}
+	public void updateLigne(int numLigne, Object[] ligne)
+    {
+        Object matrice [][] = new Object[this.donnees.length][this.entetes.length];
+        for(int i = 0; i<this.donnees.length; i++) {
+            if(i!= numLigne){
+                matrice[i] = this.donnees[i];
+            }else {
+                matrice[i] = ligne;
+            }
+        }
+        this.donnees = matrice ; //recopie de matrices.
+        this.fireTableDataChanged(); // mise à jour des données. 
+    }
+	public void deleteLigne (int numLigne)
+    {
+    Object matrice [][] = new Object[this.donnees.length -1][this.entetes.length];
+    int j = 0;
+        for(int i = 0; i<this.donnees.length; i++)
+        {
+            if(i!= numLigne){
+                matrice[j] = this.donnees[i];
+                j++;
+            }
+
+        }
+        this.donnees = matrice ; //recopie de matrices.
+        this.fireTableDataChanged(); // mise à jour des données.
+    }
+    public void setDonnees (Object donnees[][])
+    {
+        this.donnees = donnees ; //recopie de matrices.
+        this.fireTableDataChanged(); // mise à jour des données. 
+    }
 }
